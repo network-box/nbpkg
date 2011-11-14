c8937109373630d1b3e6cb8e37c39c28177d416a
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2011-11-14 05:13:03 +0000

    Project skeleton
    git-svn-id: https://10.8.18.16/nbrs/5.0@19736 ef222f69-f2fc-0310-9845-a72751d35017

---
+++ README.txt
@@ -0,0 +1,9 @@
+This is nbpkg, a tool to interact with the Network Box build system.
+
+It is based on the rpkg framework by Jesse Keating, and largely inspired by the
+fedpkg tool from the same author:
+    https://fedorahosted.org/rpkg/
+    https://fedorahosted.org/fedora-packager
+
+The tool should be self-documented, just run it without any argument to see the
+available sub-commands and read the help from that.
---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, command
+
+setup(
+    name = "nbpkg",
+    version = "0.1",
+    author = "Mathieu Bridon",
+    author_email = "mathieu.bridon@network-box.com",
+    description = "Utility to interact with the Network Box Packaging system",
+    license = "GPLv2+",
+    url = "https://www.network-box.com",
+    package_dir = {'': 'src'},
+    packages = ['pyrpkg/nbpkg'],
+    scripts = ['src/nbpkg'],
+    data_files = [('/etc/bash_completion.d', ['src/nbpkg.bash']),
+                  ('/etc/rpkg/', ['src/nbpkg.conf', 'src/nbpkg-nonfree.conf']),
+                  ],
+)
