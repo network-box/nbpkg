e279c1bc676a10826204062af435c1a28a660aa2
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-06-21 10:26:20 +0000

    Bump SVN revision for prerelease
    git-svn-id: https://10.8.18.16/nbrs/5.0@22404 ef222f69-f2fc-0310-9845-a72751d35017

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, command
 
 setup(
     name = "nbpkg",
-    version = "5.0.1-svn21787",
+    version = "5.0.1-svn22403",
     author = "Mathieu Bridon",
     author_email = "mathieu.bridon@network-box.com",
     description = "Utility to interact with the Network Box Packaging system",
