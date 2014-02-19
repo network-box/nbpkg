ac669d45b5c9e0608ac814f0a519e57fcb57371a
Mathieu Bridon <bochecha@fedoraproject.org>
2014-02-19 18:27:53 +0800

    setup: Trivial changes
    This is just to make it easier to compare with fedpkg.

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ setup(
     version = "5.0.2",
     author = "Mathieu Bridon",
     author_email = "mathieu.bridon@network-box.com",
-    description = "Utility to interact with the Network Box Packaging system",
+    description = ("Network Box plugin to rpkg to manage "
+                   "package sources in a git repository"),
     license = "GPLv2+",
     url = "https://github.com/network-box/nbpkg",
     package_dir = {'': 'src'},
     packages = ['pyrpkg/nbpkg'],
     scripts = ['src/nbpkg'],
     data_files = [('/etc/bash_completion.d', ['src/nbpkg.bash']),
-                  ('/etc/rpkg/', ['src/nbpkg.conf', 'src/nbpkg-nonfree.conf']),
+                  ('/etc/rpkg', ['src/nbpkg.conf', 'src/nbpkg-nonfree.conf']),
                   ],
 )
